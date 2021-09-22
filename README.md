ddd99bba3324af49e3ab6c7aff302c7f57defa96
Benjamin Joly <benjamin.joly@live.fr>
2021-09-22 14:50:09 +0200

    Base

---
+++ README.md
@@ -0,0 +1,11 @@
+Evaluation - Génie logiciel - Test
+
+## Installation
+
+Install Composer dependencies
+
+    composer install
+
+## Run the tests
+
+    ./vendor/bin/phpunit tests
---
+++ tests/romanTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class romanTest extends TestCase
+{
+    public function testIsDecimalToRoman()
+    {
+       
+    }
+
+
+    public function testNonDecimalToRoman()
+    {
+
+    }
+}
